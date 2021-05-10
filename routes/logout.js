e4dce1e69bd2e7116d8ad5f856cbeb47cb2b4d9f
Mert <57390427+Egm1803@users.noreply.github.com>
2021-05-10 23:34:07 +1000

    add logout to github

--- routes/logout.js
+++ routes/logout.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const auth = require('../middleware/auth');
 const asyncMiddleware = require('../middleware/asyncMiddleware');
-
+ 
 router.get('/', auth, asyncMiddleware(async function(req, res, next) {
     //To hide the Fill Out The Form link in layout bar
     res.clearCookie('my_session');
